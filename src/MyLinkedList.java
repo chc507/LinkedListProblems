c82e3c662961f01926811cdb5d4361325bb64e1e
vannesschancc <vannesschan1993@gmail.com>
2017-12-08 01:00:20 -0800

    Add LinkedList basis

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 public class MyLinkedList {
     ListNode head = null;
+
     //insert
     public void addNode(int val) {
         if (head == null) {
@@ -49,7 +50,7 @@ public class MyLinkedList {
     private int getLength() {
         int length = 0;
         ListNode pointer = head;
-        while  (pointer != null) {
+        while (pointer != null) {
             length++;
             pointer = pointer.next;
         }
@@ -77,7 +78,7 @@ public class MyLinkedList {
             next = current.next;
             while (next != null) {
                 if (next.val > current.val) {
-                    swap(current,next);
+                    swap(current, next);
                 }
                 next = next.next;
             }
@@ -95,7 +96,7 @@ public class MyLinkedList {
     //Delele duplicate
     //Use Map to check if there is duplicate <- Save time
     public void removeDuplicate(ListNode head) {
-        Map<Integer,Boolean> map = new HashMap<>();
+        Map<Integer, Boolean> map = new HashMap<>();
         ListNode current = head;
         ListNode prev = null;
         while (current != null) {
@@ -110,11 +111,103 @@ public class MyLinkedList {
         }
     }
 
-    // double iteration to save  space
+    // double iteration to save  space, but O(n)
     public void removeCompact(ListNode head) {
-        ListNode pointer = head;
-
+        ListNode p = head;
+        ListNode q;
+        while (p != null) {
+            q = p;
+            while (q.next != null) {
+                if (q.next.val == p.val) {
+                    q.next = q.next.next;
+                } else {
+                    q = q.next;
+                }
+            }
+            p = p.next;
+        }
     }
 
 
+    //PrintListReverse
+    public void printListReverse(ListNode head) {
+        if (head != null) {
+            printListReverse(head.next);
+            System.out.print(head.val + " ");
+        }
+    }
+
+    //The last K elements in LinkedList
+    //traverse the list twice, the first time, get length, the second time, find n - k element
+    //simplify as using two pointer, sp, fp, fp =  sp + k - 1; when the fast pointer reach the end,
+    //return the slow pointer
+
+    public ListNode findLastKNodes(ListNode head, int k) {
+        if (k < 1 || k > getLength()) {
+            return null;
+        }
+
+        ListNode p1 = head;
+        ListNode p2 = head;
+
+        for (int i = 0; i < k; i++) {
+            p1 = p1.next;
+        }
+
+        while (p1 != null) {
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        return p2;
+    }
+
+
+    //Find the mid point of the singly linked-list
+    ///two pointer, one fast, one slow
+    public ListNode getMidNode(ListNode head) {
+        ListNode p = head; //fast
+        ListNode q = head;
+        while (p != null && p.next != null && p.next.next == null) {
+            q = q.next;
+            p = p.next.next;
+        }
+        return q;
+    }
+
+    //Reverse Linkedlist
+
+    public ListNode reverseList(ListNode head) { //check stands. then move to next
+        ListNode newHead = head;
+        ListNode current = head;
+        ListNode prev = null;
+
+        while (current != null) {
+            //TODO: lOOK AT HERE, YOU
+            ListNode next = current.next;
+            if (next == null) { //reach the end;
+                newHead = current;
+            }
+            current.next = prev;
+            //move to next;
+            prev = current;
+            current = next;
+        }
+        return newHead;
+    }
+
+
+    //Delete certain nodes without knowing the head node
+    //main issue, you can not have previous node
+    //if deleted point is the last node, you can not get the pointer to it. you can not have prev
+    public boolean deleteNodeWoHead(ListNode n){
+        if (n == null || n.next == null) {
+            return false;
+        } else {
+            int temp = n.val;
+            n.val = n.next.val;
+            n.next.val = temp; //swap data n and n+1
+            n.next = n.next.next;
+            return true;
+        }
+    }
 }
